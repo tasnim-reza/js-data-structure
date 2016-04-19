cf10e12e519e6a555d6dfe78932448fdecdcbb9a
thmehorizon <thmeshoot@gmail.com>
2016-04-19 09:02:15 +0600

    working on graph bfs

--- src/graph/bfs/bfs.js
+++ src/graph/bfs/bfs.js
@@ -1,3 +1,46 @@
 /**
  * Created by tasnim.reza on 17-Apr-16.
  */
+
+function bfsPrintAllNodes(graph, startNode, endNode) {
+    var queue = new Queue();
+    var visited = []
+    visited.push(startNode);
+    while (startNode) {
+        console.log(startNode);
+
+        for (var i = 0; i < graph[startNode].length; i++) {
+            var currentNode = graph[startNode][i];
+            if (visited.indexOf(currentNode) === -1) {
+                visited.push(currentNode);
+                queue.enqueue(currentNode);
+            }
+        }
+
+        startNode = queue.dequeue();
+    }
+}
+
+(function () {
+    var graph = {
+        'a': ['b', 'd'],
+        'b': ['a', 'd', 'c'],
+        'c': ['b', 'd', 'f'],
+        'd': ['a', 'b', 'c', 'f', 'e'],
+        'e': ['d'],
+        'f': ['c', 'd']
+    };
+
+    /*var graph = {
+     'a': ['d'],
+     'b': ['a', 'd', 'c'],
+     'c': ['d', 'f'],
+     'd': ['e'],
+     'e': ['d'],
+     'f': [ 'd']
+     };*/
+
+    bfsPrintAllNodes(graph, 'b', 'e');
+
+    //bfsPrintAllPaths(graph, 'b', 'e');
+})();
--- src/graph/dfs/dfs.js
+++ src/graph/dfs/dfs.js
@@ -4,32 +4,79 @@
 
 //find shortest path using dfs
 
-function dfs(graph, startNode, endNode) {
+//print all connected nodes
+function dfsPrintAllNodes(graph, startNode, endNode) {
     var stack = [],
         keys = Object.keys(graph);
 
     recurse(startNode, stack);
 
     function recurse(node, stack) {
+        console.log('visited ', node);
         for (var i = 0; i < graph[node].length; i++) {
-            if (stack.indexOf(node) === -1) {
+            var currentNode = graph[node][i];
+
+            if (stack.indexOf(currentNode) === -1) {
                 stack.push(node);
-                recurse(graph[node][i], stack)
+                recurse(currentNode, stack)
             }
         }
-        console.log(stack.pop());
+    }
+}
+
+function dfsPrintAllPaths(graph, startNode, endNode) {
+    var stack = [],
+        paths = [],
+        keys = Object.keys(graph);
+    var count = 0;
+    recurse(startNode, stack);
+    console.log(paths);
+
+    function recurse(node, stack) {
+        console.log(stack, count++);
+        stack.push(node);
+
+        if (node === endNode) {
+            paths.push('-')
+            stack.forEach(function(p){
+                paths.push(p);
+            });
+
+            console.log('path no ', stack);
+        }
+
+        for (var i = 0; i < graph[node].length; i++) {
+            var currentNode = graph[node][i];
+            console.log(stack, count++);
+            if (stack.indexOf(currentNode) === -1) {
+                recurse(currentNode, stack)
+            }
+        }
+
+        stack.pop();
     }
 }
 
 (function () {
     var graph = {
         'a': ['b', 'd'],
-        'b': ['a', 'd'],
-        'c': ['b', 'd'],
+        'b': ['a', 'd', 'c'],
+        'c': ['b', 'd', 'f'],
         'd': ['a', 'b', 'c', 'f', 'e'],
         'e': ['d'],
         'f': ['c', 'd']
     };
 
-    dfs(graph, 'a', 'e');
+    /*var graph = {
+     'a': ['d'],
+     'b': ['a', 'd', 'c'],
+     'c': ['d', 'f'],
+     'd': ['e'],
+     'e': ['d'],
+     'f': [ 'd']
+     };*/
+
+    //dfsPrintAllNodes(graph, 'b', 'e');
+
+    dfsPrintAllPaths(graph, 'b', 'e');
 })();
